b7f4df2d12629cefdd54074561e8614560a5910f
mateushrc <mat3us.hrc@gmail.com>
2025-11-14 11:01:10 -0300

    Initial commit

--- obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78f2ca28b82ddf1bc67ed33402d1cdfb9f5a6970")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
